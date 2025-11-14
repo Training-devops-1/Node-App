6887d346c721c8980ecd5d621aded1b919ee275e
Prince Robin <princerobinraj@gmail.com>
2025-11-14 15:31:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello World from Node"));
+app.get("/", (req, res) => res.send("Hello World from Node.js"));
 app.listen(3020, () => console.log("Server running on port 3020"));
